784cf5c2e3324e82b820f092e12ca4f97275ac56
Sebastian Twarog <contact@stwarog.com>
2022-01-13 15:33:36 +0100

    Fixed class name generation

--- src/NameGenerator.php
+++ src/NameGenerator.php
@@ -6,11 +6,19 @@ namespace Stwarog\FuelFixturesGenerator;
 
 /**
  *  This class generates new Fixture class name basing on Fuel`s related model name.
- *  E.g: relation can be Orm_Model_Currency, and we want to generate fixture class named Orm_Model_Currency_Fixture
+ *  E.g: relation can be Orm_Model_Currency, and we want to generate fixture class named OrmModelCurrencyFixture.
+ *
+ *  Each method value should start from "\"
  */
 interface NameGenerator
 {
+    /**
+     * Should attach namespace to newly generated short name
+     */
     public function getFullName(string $class): string;
 
+    /**
+     *  Should contain simple class name without namespace
+     */
     public function getShortName(string $class): string;
 }
